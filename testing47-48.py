e7a21d2d4a261b20646784a3284425db7d6a4b57
kavitapatil241220 <kavitapatil241220@gmail.com>
2023-10-26 20:45:45 +0530

    branch change

--- testing47-48.py
+++ testing47-48.py
@@ -1,4 +1,7 @@
 a = int(input("Enter the value of A"))
 b = int(input("Enter the value of B"))
 c = a - b
-print("Addition of A and B is:", c)
+print("Subtraction of A and B is:", c)
+
+d= a+b
+print("addition of A and B is:", d)
